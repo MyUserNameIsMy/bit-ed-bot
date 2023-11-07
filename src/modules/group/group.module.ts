b5c87b0474d5ce978d7e77f1dbcfe41fab083c39
myusernameismy <ilyas123.822@gmail.com>
2023-11-07 21:04:46 +0600

    fix orm

--- src/modules/group/group.module.ts
+++ src/modules/group/group.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
 import { ClientTutorEntity } from './entities/client-tutor.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ClientTutorEntity],
+  imports: [TypeOrmModule.forFeature([ClientTutorEntity])],
   controllers: [GroupController],
   providers: [GroupService],
 })
